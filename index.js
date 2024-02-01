f5c7a271b4f39e9f97c637ea40937369da7a4963
yolo <igoncharov@flightpath3d.com>
2024-02-01 18:29:48 +0000

    feat: 'goodbye' msg, basic readline interface

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import { createInterface } from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 import { username } from './src/username.js';
 
-console.log(`Welcome to the File Manager,, ${username}!`);
+console.log(`Welcome to the File Manager, ${username}!`);
+
+const rl = createInterface({ input, output });
+
+rl.on('line', line => {
+  console.log(`You entered: ${line}`);
+})
+
+rl.on('close', () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+})
