fbdd6cc22bd7019c3f06a20eaae6771a8a62ed8b
yolo <igoncharov@flightpath3d.com>
2024-02-02 20:18:19 +0000

    feat: input-processing - new statements

--- src/input-processing.js
+++ src/input-processing.js
@@ -1,11 +1,22 @@
 import { II } from "./constants/error.js"
-import { navUp } from "./navigator.js"
+import { ls, add } from "./fs.js";
+import { navUp, cd } from "./navigator.js"
 
-export const processInput = (command) => {
+export const processInput = async (command) => {
   console.log(`Command: ${command}`);
-  switch (command) {
-    case 'up':
-      navUp()
+  switch (true) {
+    case 'up' === command:
+      navUp();
+      break;
+    case command.startsWith('cd '):
+      cd(command.slice(3));
+      break;
+    case 'ls' === command:
+      await ls();
+      break;
+    case command.startsWith('add'):
+      await add(command.slice(3));
+      break;  
     default:
       console.log(II)
   }
