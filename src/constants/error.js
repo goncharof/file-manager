cb65c8fbeb900771568fed1ec02ff038104728a7
yolo <igoncharov@flightpath3d.com>
2024-02-05 13:26:32 +0000

    feat: ERROR_COLOR const

--- src/constants/error.js
+++ src/constants/error.js
@@ -2,3 +2,6 @@ export const OF = 'Operation failed'
 export const DNE = 'directory does not exist'
 export const II = 'Invalid input'
 export const AE = 'File already exists'
+
+
+export const ERROR_COLOR = '\x1b[31m%s\x1b[0m'
