c2dd428725baa0326fbd99872974b28bcd7bca03
yolo <igoncharov@flightpath3d.com>
2024-02-05 13:26:56 +0000

    feat: input-rocessing ext

--- src/input-processing.js
+++ src/input-processing.js
@@ -1,23 +1,48 @@
-import { II } from "./constants/error.js"
-import { ls, add } from "./fs.js";
+import { II, ERROR_COLOR } from "./constants/error.js"
+import { ls, add, rn, cp } from "./fs.js";
 import { navUp, cd } from "./navigator.js"
+import { osProcessing } from "./os-processing.js";
 
 export const processInput = async (command) => {
   console.log(`Command: ${command}`);
+  switch (command) {
+    case '.exit':
+      return false;
+      case 'up':
+        navUp();
+        break;
+      case 'ls':
+        await ls();
+        break;
+  }
+  
+  console.log(123);
+
   switch (true) {
-    case 'up' === command:
-      navUp();
-      break;
     case command.startsWith('cd '):
       cd(command.slice(3));
       break;
-    case 'ls' === command:
-      await ls();
+    case command.startsWith('add '):
+      await add(command.slice(4));
       break;
-    case command.startsWith('add'):
-      await add(command.slice(3));
-      break;  
+    case command.startsWith('rn '):
+      await rn(command.slice(3));
+      break;
+    case command.startsWith('cp '):
+      await cp(command.slice(3));
+      break;
+    case command.startsWith('mv '):
+      await cp(command.slice(3));  
+      break;
+    case command.startsWith('rm '):
+      await rm(command.slice(3));
+      break; 
+    case command.startsWith('os '):      
+      osProcessing(command.slice(3));
+      break;
     default:
-      console.log(II)
+      console.error(ERROR_COLOR, II)
   }
+
+  return true;
 }
